7199188d14c65c3936438d0131116e7cc5856c20
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-10-18 16:48:58 +0200

    add variable argument lists

--- lua_spec.lua
+++ lua_spec.lua
@@ -105,6 +105,33 @@ describe('Function', function()
 end)
 
 
+describe('A variable argument list', function()
+    it('sadly uses ... syntax for definition and reference', function()
+        function f(...)
+            return ...
+        end
+        x, y = f(1, 2)
+        assert(x == 1 and y == 2)
+    end)
+
+    it('sadly is interpolated in assignments', function()
+        function f(...)
+            x, y, z = 'first', ...
+            return z
+        end
+        assert(f('second', 'third') == 'third')
+    end)
+
+    it('sadly is interpolated in table constructors', function()
+        function f(...)
+            table = {'first', ...}
+            return table[3]
+        end
+        assert(f('second', 'third') == 'third')
+    end)
+end)
+
+
 describe('Function arguments', function()
     it('are passed by reference', function()
         function f(table)
