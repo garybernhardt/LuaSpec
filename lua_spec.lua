ae6657d199acc74756bd2d884791d67f61106173
Gary Bernhardt <gary.bernhardt@gmail.com>
2010-10-18 13:27:05 +0200

    add assignments

--- lua_spec.lua
+++ lua_spec.lua
@@ -26,6 +26,44 @@ function assert_raises(callable)
 end
 
 
+describe('Assignment', function()
+    it('sadly throws away extra values', function()
+        x, y = 1, 2, 3
+        assert(x == 1 and y == 2)
+    end)
+
+    it('sadly fills in missing values with nils', function()
+        x, y = 1
+        assert(x == 1 and y == nil)
+    end)
+
+    it('sadly interpolates calls into the value list', function()
+        local f = function() return 2, 3 end
+        x, y, z = 1, f()
+        assert(x == 1)
+        assert(y == 2)
+        assert(z == 3)
+    end)
+
+    it("sadly swallows extra values in parenthesized calls", function()
+        local f = function() return 2, 3 end
+        x, y, z = 1, (f())
+        assert(x == 1)
+        assert(y == 2)
+        assert(z == nil)
+    end)
+
+    it('evaluates the values before assigning', function()
+        a = {}
+        i = 3
+        i, a[i] = i + 1, 20
+        assert(i == 4)
+        assert(a[3] == 20)
+        assert(a[4] == nil)
+    end)
+end)
+
+
 describe('Number', function()
     it('is addable', function()
         assert(1 + 1 == 2)
